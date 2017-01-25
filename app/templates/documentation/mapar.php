22402425501053a6e6f34e90f777ce4990091e73
Etudiant <Etudiant>
2017-01-25 16:14:24 +0100

    création du dossier pdf dans assets

--- app/templates/documentation/mapar.php
+++ app/templates/documentation/mapar.php
@@ -1,8 +1,75 @@
-<?php $this->layout('layout', ['title' => 'Présentation Mapar']) ?>
+<?php $this->layout('layoutType', ['title' => 'Présentation Mapar']) ?>
 
 <?php $this->start('main_content') ?>
 	<h1>Présentation MAPAR</h1>
 
+	<section>
+		
+		<button class="accordion"><h2>Présentation 2010</h2></button>
+		<div class="panel">
+			<p>Nouvelle Gouvernance = l'expérience Roannaise (Dr Jean-Claude Ducreux- Roanne)
+			</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+
+			<p>Dr James Brodeur-Bourges</p>
+			<p>Modif statut=Indépendance Professionnelle des Médecins Hospitaliers</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+		</div>
+
+		<button class="accordion"><h2>Présentation 2009</h2></button>
+		<div class="panel">
+			<p>Nouvelle Gouvernance = l'expérience Roannaise (Dr Jean-Claude Ducreux- Roanne)
+			</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+
+			<p>Dr James Brodeur-Bourges</p>
+			<p>Modif statut=Indépendance Professionnelle des Médecins Hospitaliers</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+		</div>
+
+		<button class="accordion"><h2>Présentation 2008</h2></button>
+		<div class="panel">
+			<p>Nouvelle Gouvernance = l'expérience Roannaise (Dr Jean-Claude Ducreux- Roanne)
+			</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+
+			<p>Dr James Brodeur-Bourges</p>
+			<p>Modif statut=Indépendance Professionnelle des Médecins Hospitaliers</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+		</div>
+
+		<button class="accordion"><h2>Présentation 2007</h2></button>
+		<div class="panel">
+			<p>Le SMARNU et l'Europe- Assemblée Générale de la FEMS (Dr M. Vignier)
+			</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+
+			<p>Impact des modifications statutaires sur la fonction de PH (Dr Denis Cazaban-Montfermeil)</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+		</div>
+
+		<button class="accordion"><h2>Présentation 2006</h2></button>
+		<div class="panel">
+			<p>EPP et accréditation, quelles méthodologies? (Drs Pierre Perucho, et Danièle Goumard)
+			</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+
+			<p>Points d’IRCANTEC : point(s) de retraites ? Dr P.-A. Doutre (Belfort)</p>
+			<a href="#" target="_blank">Télécharger le PDF</a>
+		</div>
+
+		<button class="accordion"><h2>Présentation 2005</h2></button>
+		<div class="panel">
+			<p>Dr Jean-Claude Ducreux- Roanne
+			</p>
+			<a href="#" target="_blank">Nouvelle Gouvernance = l'expérience Roannaise </a>
+
+			<p>Dr James Brodeur-Bourges</p>
+			<p></p>
+			<a href="#" target="_blank">Modif statut=Indépendance Professionnelle des Médecins Hospitaliers</a>
+		</div>
+
+	</section>
 	
 
 <?php $this->stop('main_content') ?>
--- app/templates/smarnu/lettreSyndicale.php
+++ app/templates/smarnu/lettreSyndicale.php
@@ -2,19 +2,20 @@
 
 <?php $this->start('main_content') ?>
 
-<section>
-	<h1>Lettre syndicale</h1>
+	<section>
+		<h1>Lettre syndicale</h1>
 
-	<?php
+		<?php
 		foreach ($articles as $article) : ?>
 
-			<button class="accordion"><h2><?= $this->e($article['titre']) ?></h2></button>
-				<div class="panel">
-  					<p><?= $this->e($article['contenu']) ?></p>
-  					<a href="<?= $this->assetUrl('uploads/'.$this->e($article['fichier']).''); ?>" target="_blank">Télécharger le PDF</a>
-				</div>
-			
-		<?php endforeach ?>
+		<button class="accordion"><h2><?= $this->e($article['titre']) ?></h2></button>
+		<div class="panel">
+			<p><?= $this->e($article['contenu']) ?></p>
+			<a href="<?= $this->assetUrl('uploads/'.$this->e($article['fichier']).''); ?>" target="_blank">Télécharger le PDF</a>
+		</div>
+
+	<?php endforeach ?>
 
 	</section>
+	
 <?php $this->stop('main_content') ?>
