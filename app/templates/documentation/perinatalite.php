d1c52365d734ed3cac9503bad17899e8f8788067
Etudiant <villard.edith@gmail.com>
2017-01-25 16:26:24 +0100

    perinatalite

--- app/templates/documentation/perinatalite.php
+++ app/templates/documentation/perinatalite.php
@@ -1,6 +1,27 @@
-<?php $this->layout('layout', ['title' => 'Périnatalité']) ?>
+<?php $this->layout('layoutType', ['title' => 'Périnatalité']) ?>
 
 <?php $this->start('main_content') ?>
-	
-<p>Page 'périnatalité' en construction</p>
+	<h1>Périnatalité</h1>
+	<section>
+		<h2>Textes réglementaires et rapports</h2>
+		<ul>
+			<li><a href="http://www.smarnu.org/images/para/decret-98-899-9-octobre-1998.pdf" target="_blank">Décret no 98-899 du 9 octobre 1998 (dit "décret périnat" ) </a>9 octobre 1998</li>
+			<li><a href="http://www.smarnu.org/images/para/arrt-25-avril-2000.pdf" target="_blank">Arrêté du 25 avril 2000</a> 20 avril 2000</li>
+			<li><a href="" target="_blank">Décret n° 2003-992 du 16 octobre 2003 </a> relatif aux établissements de santé publics et privés pratiquant l’obstétrique, la néonatologie, la réanimation néonatal - 16 octobre 2003</li>
+		</ul>
+
+		<h2>Textes SFAR et SNMARHNU</h2>
+		<ul>
+			<li><a href="http://www.smarnu.org/images/para/texte-commun-sfar-cngof-9-2000.pdf" target="_blank">Texte commun SFAR-CNGOF (9-2000)</a>septembre 2000</li>
+			<li><a href="http://www.smarnu.org/images/para/Decrets-prinatalit-propositions-sfar-1-2003.pdf" target="_blank">Décrets périnatalité (propositions SFAR 1-2003)</a>janvier 2003</li>
+			<li><a href="" target="_blank"></a>janvier 2003</li>
+			<li><a href="" target="_blank"></a>18 mars 2003</li>
+		</ul>
+
+		<h2>Avis juridique</h2>
+
+		<h2>Groupe de travail périnatalité</h2>
+
+		<h2>Plan perinatalité (2005-2007)</h2>
+	</section>
 <?php $this->stop('main_content') ?>
