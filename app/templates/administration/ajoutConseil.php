beec848d40d4a7e45e86c76cc87689b601fdfe08
marc mura <marc mura>
2017-01-27 12:52:01 +0100

    Intégration conseil d'administration

--- app/templates/administration/ajoutConseil.php
+++ app/templates/administration/ajoutConseil.php
@@ -45,7 +45,7 @@
 				<option value="vice-president">Vice Président</option>
 				<option value="secretaire-general">Secrétaire général</option>
 				<option value="tresorier">Trésorier</option>
-				<option value="membre">>Membre</option>
+				<option value="membre">Membre</option>
 				<option value="membre-honoraire">Membre honoraire</option>
 				<option value="president-honneur">Président d'honneur</option>
 			</select>
