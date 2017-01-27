9ce4827f8deb07a86bd30979051fe79f16730b1c
Etudiant <Etudiant>
2017-01-27 14:30:50 +0100

    ajout et modification de rttCet.php

--- app/templates/documentation/rttCet.php
+++ app/templates/documentation/rttCet.php
@@ -1,6 +1,36 @@
-<?php $this->layout('layout', ['title' => 'R.T.T / C.E.T.']) ?>
+<?php $this->layout('layoutType', ['title' => 'R.T.T / C.E.T.']) ?>
 
 <?php $this->start('main_content') ?>
-	
-<p>Page 'R.T.T / C.E.T.' en construction</p>
+	<h1></h1>
+
+	<section>
+		
+		
+		Textes CET DE 2008
+
+		Décret n° 2008-455 du 14 mai 2008 relatif aux modalités d'indemnisation des jours accumulés sur le compte épargne-temps.
+
+		Arrêté du 14 mai 2008 fixant le montant de l'indemnisation des jours accumulés sur leur compte épargne-temps par les personnels médicaux;
+
+		Arrêté du 14 mai 2008 fixant le montant de l'indemnité versée aux ayants droit lors du décès d'un praticien titulaire d'un compte épargne-temps;
+
+		Circulaire relative à l'application du décret n°2008-455 du 14 mai 2008;
+
+		Etat des lieux des provisions pour CET des personnels médicaux (région Centre)
+
+		Textes règlementaires et rapports
+
+		Cadrage National du 22 Octobre 2001
+		Fiches Techniques du ministère sur l’ARTT (novembre 2001)
+		Circulaire DHOS/M2/2002/N° 58 du 30 janvier 2002 de mise en oeuvre de l’accord sur l’aménagement et la réduction du temps de travail du 22 octobre 200
+		Circulaire DHOS du 24 juillet 2002 autorisant la prise des RTT
+		Circulaire de la DHOS du 26 juillet 2002 (répartition inter et intra régionale des crédits RTT)
+		Décret du 7 octobre (JO du 8 octobre 2002) relatif à la RTT des personnels médicaux
+		Arrêté du 17 octobre (JO du 20 octobre 2002) relatif à la RTT des personnels médicaux
+		Décret du 18 novembre 2002 portant création du CET (JO du 19 novembre 2002)
+		Rapport de la Mission d’Evaluation de la Mise en Place de la RTT dans les Etablissements de Santé (dit rapport Piquemal) (8 novembre 2002)
+		Accord assouplissement RTT du 10 janvier 2003
+		Décrets et Arrêté du 9 octobre 2003 (J.O. du 12 octobre) modifiant la RTT et le CET par les mesures d’assouplissement de février 2003
+
+	</section>
 <?php $this->stop('main_content') ?>
