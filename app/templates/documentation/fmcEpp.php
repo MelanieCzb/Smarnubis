6f63c676a2e545de82fae0f909fe68cb776d5e5f
Etudiant <Etudiant>
2017-01-27 15:41:07 +0100

    ajout et modifs des fichiers fmcEpp.php / rttCet.php + ajout de pdf dans le dossier asset/pdf

--- app/templates/documentation/fmcEpp.php
+++ app/templates/documentation/fmcEpp.php
@@ -1,6 +1,45 @@
-<?php $this->layout('layout', ['title' => 'F.M.C. - E.P.P.']) ?>
+<?php $this->layout('layoutType', ['title' => 'F.M.C. - E.P.P.']) ?>
 
 <?php $this->start('main_content') ?>
-	
-<p>Page 'F.M.C. - E.P.P.' en construction</p>
+	<h1>FMC - EPP</h1>
+
+	<section>
+		
+		<h2>EVALUATION DES PRATIQUES PROFESSIONNELLES, FORMATION MEDICALE CONTINUE, ACCREDITATION DES PRATICIENS, QUE RETENIR ?</h2>
+		
+		<p>Ci-dessous, vous trouverez :</p>
+		<p>des textes d’ exemples nord-américains (ACCME),</p>
+		<p>F.M.C. = Textes Officiels + Présentation Power Point par le Conseil National de la FMC,</p>
+		<p>E.P.P. =  Textes et analyses.</p>
+
+		<h2>Exemples étrangers</h2>
+		
+		<p><a href="<?= $this->assetUrl('pdf/davis_jama.pdf') ?>">Canada: Impact of Continuing Medical Education</a> - J.A.M.A.(1999); 282 = 867-875</p>
+		<p><a href="<?= $this->assetUrl('pdf/105_Recognition_Requirement_of_the_ACCME_0_4_04.pdf') ?>">USA: Recognition Requirements of the ACCME</a> (4/2/04)</p>
+		
+		<h2>Formation Médicale Continue</h2>
+		
+		<p>Décret n°2006-653 du 2 juin 2006 relatif à <a href="<?= $this->assetUrl('pdf/decret-2006-653-2-06-06-EPP-conseil-reg-fmc.pdf') ?>">l’évaluation des pratiques professionnelles (conseil régional de la FMC)</a></p>
+
+		<p>Arrêté du 13 juillet 2006 portant <a href="<?= $this->assetUrl('pdf/fmc-regles-09-08-2006.pdf') ?>">homologation des règles de validation de la formation médicale continue</a></p>
+		  
+		<h2>Evaluation des pratiques professionnelles</h2>
+
+		<p>Consulter aussi : <a href="http://www.has-sante.fr/portail/">has-sante</a></p>
+		
+		<a href="<?= $this->assetUrl('pdf/EPP-vu-par-HAS-juin05.pdf') ?>">DOSSIER DE PRESSE de la Haute Autorité de santé </a>– <p>Évaluation des pratiques professionnelles : Mise en oeuvre et perspectives 29 juin 2005</p>
+		<a href="<?= $this->assetUrl('pdf/Rap_activ_HAS_1semestre_2005.pdf') ?>">Rapport d’activité de la HAS au Parlement et au Gouvernement</a> 
+		<p>1 juillet 2005</p>
+
+		<h2>Présentation diapo de l'EPP et de l'accreditation du praticien mai 2006</h2>
+
+		<p>Présentation faite au MAPAR 2006 par Drs Goumard et Perucho sur <a href="<?= $this->assetUrl('pdf/epp-fmc-accreditation-09-06.pdf') ?>">l'EPP et l'Accréditation du Praticien avec mise à jour</a> septembre 2006</p>
+
+		<h2>Référentiels College Français des Anesthésistes-Réanimateurs</h2>
+
+		<p>Référentiel de Pratiques Professionnelles : <a href="<?= $this->assetUrl('pdf/referentiel-antb-college.pdf') ?>">Antibioprophylaxie péri-opératoire</a> juin 2005</p>
+
+		<p>Référentiel de Pratiques Professionnelles : <a href="<?= $this->assetUrl('pdf/refererentiel-douleur-college.pdf') ?>">Prise en charge de la douleur post-opératoire</a>juin 2005</p>
+
+	</section>
 <?php $this->stop('main_content') ?>
--- app/templates/documentation/rttCet.php
+++ app/templates/documentation/rttCet.php
@@ -1,36 +1,25 @@
 <?php $this->layout('layoutType', ['title' => 'R.T.T / C.E.T.']) ?>
 
 <?php $this->start('main_content') ?>
-	<h1></h1>
+	<h1> Textes RTT / CET</h1>
 
 	<section>
 		
+		<h2>Textes CET DE 2008</h2>
 		
-		Textes CET DE 2008
+		<p>Décret n° 2008-455 du 14 mai 2008</p>
+		<a href="<?= $this->assetUrl('pdf/decret-cet-2008-455-du-14-mai-2008.pdf') ?>">relatif aux modalités d'indemnisation des jours accumulés sur le compte épargne-temps.</a>
+		
+		<p>Arrêté du 14 mai 2008</p>
+		<a href="<?= $this->assetUrl('pdf/arrete-cet-14-mai-2008.pdf') ?>">fixant le montant de l'indemnisation des jours accumulés sur leur compte épargne-temps par les personnels médicaux;
+		</a>
 
-		Décret n° 2008-455 du 14 mai 2008 relatif aux modalités d'indemnisation des jours accumulés sur le compte épargne-temps.
+		<h2>Textes règlementaires et rapports</h2>
 
-		Arrêté du 14 mai 2008 fixant le montant de l'indemnisation des jours accumulés sur leur compte épargne-temps par les personnels médicaux;
-
-		Arrêté du 14 mai 2008 fixant le montant de l'indemnité versée aux ayants droit lors du décès d'un praticien titulaire d'un compte épargne-temps;
-
-		Circulaire relative à l'application du décret n°2008-455 du 14 mai 2008;
-
-		Etat des lieux des provisions pour CET des personnels médicaux (région Centre)
-
-		Textes règlementaires et rapports
-
-		Cadrage National du 22 Octobre 2001
-		Fiches Techniques du ministère sur l’ARTT (novembre 2001)
-		Circulaire DHOS/M2/2002/N° 58 du 30 janvier 2002 de mise en oeuvre de l’accord sur l’aménagement et la réduction du temps de travail du 22 octobre 200
-		Circulaire DHOS du 24 juillet 2002 autorisant la prise des RTT
-		Circulaire de la DHOS du 26 juillet 2002 (répartition inter et intra régionale des crédits RTT)
-		Décret du 7 octobre (JO du 8 octobre 2002) relatif à la RTT des personnels médicaux
-		Arrêté du 17 octobre (JO du 20 octobre 2002) relatif à la RTT des personnels médicaux
-		Décret du 18 novembre 2002 portant création du CET (JO du 19 novembre 2002)
-		Rapport de la Mission d’Evaluation de la Mise en Place de la RTT dans les Etablissements de Santé (dit rapport Piquemal) (8 novembre 2002)
-		Accord assouplissement RTT du 10 janvier 2003
-		Décrets et Arrêté du 9 octobre 2003 (J.O. du 12 octobre) modifiant la RTT et le CET par les mesures d’assouplissement de février 2003
+		<a href="<?= $this->assetUrl('pdf/CadrageARTT-Final-22-10-01.pdf') ?>">Cadrage National</a>
+		<p>22 Octobre 2001</p>
+		<a href="<?= $this->assetUrl('pdf/fiches-techniques-ARTT.pdf') ?>">Fiches Techniques du ministère sur l’ARTT</a>
+		<p>Novembre 2001</p>
 
 	</section>
 <?php $this->stop('main_content') ?>
