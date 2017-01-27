97e544309b679b04179f27c16c3563829579c3bc
Etudiant <Etudiant>
2017-01-27 10:59:44 +0100

    ajout et modifs des pages comStatutaires et partenaires

--- app/templates/smarnu/partenaires.php
+++ app/templates/smarnu/partenaires.php
@@ -1,6 +1,70 @@
-<?php $this->layout('layout', ['title' => 'Partenaires']) ?>
+<?php $this->layout('layoutType', ['title' => 'Partenaires']) ?>
 
 <?php $this->start('main_content') ?>
 	
-<p>Page 'Partenaires' en construction</p>
+	<h1>Partenariats</h1>
+
+	<section>
+		
+		<button class="accordion"><h2>Conseil de l'Orde</h2></button>
+		
+		<div class="panel">
+			
+			<article>
+	
+				<h3>RECOMMANDATIONS CONCERNANT LES RELATIONS ENTRE LES ANESTHESISTE-REANIMATEURS ET CHIRURGIENS? AUTRES SPECIALISTES OU PROFESSIONNELS DE LA SANTE :</h3>
+				<p>Document adopté lors de la session du Conseil National del’Ordre des Médecins de décembre 2001
+				La Position de l' Ordre des Médecins concernant le projet de modification du Statut de P.H. (4 janvier 2005)</p>
+				<p>Statut des praticiens hospitaliers : l´Ordre sort de son silence </p>
+				<p>Sébastien Daycard-Heid </p>
+				<p>La refonte du statut des praticiens hospitaliers, qui devrait être adoptée dans la journée de demain, déplaît au Conseil national de l´Ordre des Médecins. Et Jean-Jacques Kennel, secrétaire général adjoint du CNOM, compte bien le faire savoir :</p>
+				<p>" Dans une lettre du 4 janvier 2005, nous avions déjà alerté le ministère en toute discrétion. Sans aucune réponse de leur part, ni précisions, nous sommes aujourd´hui obligés de rendre notre intervention publique. L´ordre des médecins n´oublie pas les praticiens hospitaliers ".</p>
+				<p>50000 praticiens hospitaliers sont en effet inscrits à l´Ordre.
+				La raison de cette colère inhabituelle dans les salons feutrés de l´institution ? Le protocole d´accord soumis à la signature des syndicats prévoit notamment des suppléments salariaux collectifs (primes complémentaires attribuées en fonction de leur responsabilité, de l´activité et de la pénibilité), qui pourront représenter une part supplémentaire de 15% du salaire. " Sur l´activité même du praticien, cela équivaut, selon nous, à une clause de rendement quantitatif, totalement incompatible avec l´article 97 du Code de déontologie médicale ", alerte Jean-Jacques Kennel. Cet article stipule qu´un médecin salarié ne peut en aucun cas, accepter une rémunération fondée sur des normes de productivité, de rendement horaire ou toute autre disposition qui auraient pour conséquence une limitation ou un abandon de son indépendance ou une atteinte à la qualité des soins.</p>
+				<p>Déontologique, cette question est aussi syndicale, l´Ordre manifestant ainsi un parti pris : " Notre intervention est un soutien aux syndicats qui s´opposent à cet accord, notamment l´INPH qui nous a sollicité ", explique ainsi Jean-Jacques Kennel.</p>
+				<p>Que ferait toutefois le CNOM si demain l´accord est signé ? " Avec les pouvoirs publics actuels on peut s´attendre à tout, prévient le secrétaire adjoint, mais si le protocole passe, nous sommes prêts à agir ensemble ".Deux syndicats sur quatre sont prêts à signer, la CMH et le Snam-HP. En revanche, l´INPH et la CHG y sont hostiles.</p>
+				
+			</article>
+			
+		</div>
+
+		<button class="accordion"><h2>SFAR</h2></button>
+		
+		<div class="panel">
+			
+			<article>
+				<h3>Réflexions de la spécialité d’anesthésie-réanimation sur les évolutions actuelles du système de santé francais</h3>
+				<p>Il y a quelques mois, les responsables de la spécialité d’anesthésie-réanimation ont publié un texte à propos de leur positionnement vis-à-vis des réformes en cours. Ces réformes ont pris de nouvelles directions ou se traduisent par des résultats surprenants, au moins pour nous, ce qui justifie la publication et la diffusion de ce document qui complète le premier. </p>
+				<p>Il est destiné aux Doyens, Présidents de CME, Présidents de CCM, Présidents des autres Sociétés, aux Directeurs d’établissements, Directeurs d’ARH et à la DHOS .</p>
+				<p>Valorisation de l'activité de l'anesthésie-réanimation</p>
+				<p>La CCAM dans sa version V1 et V2 est très différente de la version V0. La méthodologie qui avait servi à l’établissement pendant plusieurs années de cette CCAM était considérée par la plupart, ou en tout cas par l’écrasante majorité des spécialités, comme excellente et son objectif de corriger des distorsions de l’ancienne NGAP particulièrement approprié. Le pôle nomenclature avait même créé des instances de recours qui ont permis que les doléances des uns et des autres soient examinées, ce qui a été fait (y compris sur le plan méthodologique).</p>
+				<p>Force est de constater que des négociations ont été ensuite menées en comité restreint avec des objectifs à l’opposé de ceux qui avaient présidé à l’élaboration de la CCAM.</p>
+				<p>S’il était normal d’envisager une période transitoire pour éviter des réductions brutales des valorisations ou de nombre d’emplois médicaux suite à l’application de la CCAM, les modifications qui ont été apportées ne sont pas acceptables pour beaucoup de spécialités dont la nôtre.</p>
+				<p>Dans la mesure où cette réforme concerne aussi les établissements publics, il n’est pas acceptable que la négociation se déroule sans les représentants du public (Directeurs d’hôpitaux, Praticiens hospitaliers).</p>
+				<p>Nous demandons donc que des corrections soient apportées rapidement à la CCAM, pour revenir à l’esprit de la V0 et que les représentants du secteur public participent aussi aux discussions.
+				Application des décrets « réanimation »</p>
+				<p>Plusieurs décrets concernant la pratique de la réanimation chirurgicale sont sortis, vont sortir, ou sont encore en cours d’élaboration.</p>				 
+				<p>La réanimation post-opératoire fait partie intégrante de la prise en charge de l’opéré par l’anesthésiste-réanimateur quand la gravité du patient le justifie du fait de la présence de défaillances vitales. Les textes ne peuvent pas être dévoyés de leurs objectifs initiaux, à savoir la professionnalisation de l’activité de réanimation et la valorisation par la T2A de cette activité très coûteuse.</p>
+				<p>L’émergence de l’appellation « Soins intensifs » dans certains textes, pour des raisons non expliquées, présente des inconvénients rédhibitoires :</p>
+				<p>1. Une valorisation à la journée qui n’est pas viable financièrement ce qui aboutira à une pratique dégradée, nuisible pour la sécurité du patient (densité du personnel médical et infirmier).</p>
+				<p>2. Un risque de « déprofessionnalisation » de la prise en charge médicale car l’appellation Unité de soins intensifs autoriserait des médecins non-titulaires du DES d’anesthésie-réanimation ou du DES de réanimation médicale à prendre en charge ces patients, pour des actes qui relèvent de la réanimation.</p>
+				<p>Equipes opératoires</p>
+				<p>Le nécessaire bon fonctionnement des sites opératoires et de l’activité chirurgicale a été reconnu comme une priorité de Santé publique et des décisions ont été prises pour y parvenir. Toutefois, si la discipline d’anesthésie-réanimation est un partenaire naturel et permanent du chirurgien, son organisation relève exclusivement des responsables de l’anesthésie-réanimation, quel que soit le type de structure. Il en est de même pour toutes les spécialités.</p>
+				<p>Donc, certaines solutions, parfois évoquées au cours de réunions de travail, sont, pour notre discipline, de très mauvaises solutions, notamment celle qui consisterait à déléguer du travail médical à des non-médecins (IADE).</p>
+				<p>N’ayant pas la connaissance qu’une telle proposition ait été faite pour les autres disciplines médicales ou chirurgicales, nous n’avons pas trouvé d’argumentaire convaincant pour aller dans ce sens, qui, en l’occurrence, aurait un impact négatif sur l’avenir de notre spécialité.</p>
+				<p>Jean MARTY, Président de la SFAR</p>
+				<p>Claude MARTIN, Président du Syndicat des PU-PH d’anesthésie-réanimation </p>
+				<p>Bertrand DUREUIL, Président du CFAR </p>
+				<p>Michel LÉVY, Président du SNARF</p>
+				<p>Didier REA, Président du SNPHAR</p>
+				<p>Philippe SCHERPEREEL, Président de la Sous-Section 48-01 d’anesthésie du CNU </p>
+				<p>Michel VIGNIER, Président du SNMARHNU</p>
+	
+			</article>
+			
+		</div>
+
+	</section>
+	
+
 <?php $this->stop('main_content') ?>
