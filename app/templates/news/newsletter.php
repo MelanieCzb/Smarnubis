dfd0e6d42bc2d34968d4ff2fa1446c81b5b16aa0
marc mura <marc mura>
2017-01-30 15:24:49 +0100

    Inscription à la newsletter

--- app/templates/news/newsletter.php
+++ app/templates/news/newsletter.php
@@ -5,6 +5,32 @@
 <h1>Gestion de l'abonnement à la newsletter du SMARNUBIS</h1>
 
 	<section>
+		<?php
+				if(!empty($erreurs)){ ?>
+				<div class="row" style="background-color: red;color: white;font-weight: bold;">
+					<h3>Liste des erreurs</h3>
+					<ul>
+						<?php 
+						foreach($erreurs as $erreur) { ?>
+							<li><?= $erreur ?></li>
+						<?php } ?>
+					</ul>
+				</div>
+			<?php }
+
+				if(!empty($messages)){ ?>
+				<div class="row" style="background-color: green;color: white;font-weight: bold;">
+					<ul>
+						<?php 
+						foreach($messages as $message) { ?>
+							<li><?= $message ?></li>
+						<?php } ?>
+					</ul>
+				</div>
+			<?php }
+
+		?>
+
 		<h2>Inscription</h2>
 		<form method="POST" action="">
 			<input type="text" name="myform[email]" placeholder="Votre email">
